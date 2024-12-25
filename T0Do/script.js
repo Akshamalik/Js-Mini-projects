2662a51ada4656d456062e6c23b1b50a8c517367
Aksha malik <aksha5u786@gmail.com>
2024-12-25 14:10:15 +0530

    todo and weather app modified

--- T0Do/script.js
+++ T0Do/script.js
@@ -1,15 +1,26 @@
+//as soon as the page loads we want the render task to local storage than store than make it display
+//crud operations and using local storage 
 document.addEventListener("DOMContentLoaded", () => {
+  //first step to grab to elements
   const todoInput = document.getElementById("todo-input"); //the todo input
   const addTaskButton = document.getElementById("add-task-btn"); //the button to add to list
   const todoList = document.getElementById("todo-list"); //the todo list ul
 
   //to store task
+  //.parse convert to original ds
   let tasks = JSON.parse(localStorage.getItem('tasks'))||[];
   tasks.forEach(task=>renderTask(task))
+
+  //when input is submitted
   addTaskButton.addEventListener("click", () => {
+    //grab the input value
     const taskText = todoInput.value.trim();
     //trim() removes the extra spaces
+
+    //if nothing return do nothing
     if (taskText === "") return;
+
+    //object 
     const newTask = {
       id: Date.now(),
       text: taskText,
@@ -17,11 +28,14 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     tasks.push(newTask);
     saveTasks();
+    renderTask(newTask)
     todoInput.value = ""; //clear input
     console.log(tasks);
   });
 
+  //render task display on dom
   function renderTask(task) {
+    //createelement to create element
     const li=document.createElement('li')
     li.setAttribute('data-id',task.id)
     if(task.completed) li.classList.add('completed')
@@ -35,10 +49,11 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     li.querySelector('button').addEventListener('click',(e)=>{
         e.stopPropagation() //event bubbling or event propagation or prevent toggle from firing
-        tasks=tasks.filter(t=>t.id !=task.id)
+        tasks=tasks.filter(t=>t.id !==task.id)
         li.remove()
         saveTasks()
     })
+    //append li to ul as child
     todoList.appendChild(li)
   }
 
